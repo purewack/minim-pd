80bbe5e0916e98457d673af273c50d7d0901e33a
Damian Nowacki <bobimaster15@gmail.com>
2022-11-13 14:31:00 +0000

    [rc505] annotation of header

--- bank.h
+++ bank.h
@@ -5,21 +5,27 @@
 // void cpppost();
 // std::string complexcpp(std::string s);
 #define MOTIF_BUF_SIZE 4194304 //4MB = 1024*1024*4
+
+//sync tick = n64 block boundary timing
+//quan tick = n*sync ticks
 typedef struct _motif{
     int state;
     int n_state;
-    bool gate;
-    bool onetime;
+    bool gate; //if play button let go, stop sound
+    bool onetime; //dont loop
+    bool synced; //honour quantize ticks
 
     int isDubbing;
-    int isLong;
+    int isLong; //if over buf size, will require disk file for stream from and to
     
-    int len_syncs;
-    int pos_syncs;
-    int len_spl;
-    int pos_spl;
-    t_float pos_ratio;   
-    int last_sync;
+    int len_syncs; //number of sync ticks loop spans
+    int pos_syncs; //playhead position in sync ticks
+    int len_spl;// loop spans in absolute samples
+    int pos_spl;// playhead position normalised to len_spl
+    t_float pos_ratio; //playhead position normalised to len_syncs 
+    int last_sync;//sync time of last tick
+
+    //buffer management
     int dataHead;
     t_sample *_aData, *_bData, *_data, *_ndata;
 } t_motif;
@@ -51,28 +57,29 @@ extern "C"{
 
         t_atom      a_m_stats[5];
         
-        int         is_active;
-        int         tick_duration;
-        int         tick_start;
-        int         tick_next;
-        int         tick_current;
-        int         tick_action_pending;
-        int         tick_action_when;
-        int         tick_action_nstate;
-        float       last_sync;
+        int         is_active;// if not, does not take any input, still processes sound
+        int         tick_current;//current tick timer, all values stem from
+        int         tick_duration;// quantize Tick
+        int         tick_start;//when the last tick started
+        int         tick_next;//when the next tick will happen
+        int         tick_action_pending;//if action is pending on tick
+        int         tick_action_when;//when action should be exec
+        int         tick_action_nstate;//next motif state on action
+        float       last_sync;//last time of sync tick
 
         t_motif**   motifs_array;
         int         motifs_array_count;
         t_motif*    active_motif_ptr;
         int         active_motif_idx;
 
-        bool        stateBtnStart;
-        bool        stateBtnStop;
+        bool        stateBtnStart; //top ctrl
+        bool        stateBtnStop; //bot ctrl
+        long        holdCounter;// + for top ctrl, - for bot ctrl
 
         pthread_t   worker_thread;
         int         worker_thread_alive;
         int         work_type; //1 refill, -1 unfill, 2 fetch 
-        int         work_type_inthread;
+        int         work_type_inthread;//internal buffer flag for thread
         pthread_cond_t  cond_work;
         pthread_mutex_t mtx_work; 
     } t_bank;
