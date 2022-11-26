32c67a13968ba8c65e472975306a9158373fc2e6
purewack <bobimaster15@gmail.com>
2022-11-26 00:11:04 +0000

    [hardware] send button midi note

--- minim_hw/minim_hw.ino
+++ minim_hw/minim_hw.ino
@@ -173,6 +173,7 @@ void setup(){
       begin_ssd1306();
       gfx.rotated = 1;
       gfx_clear();
+      gfx_drawRectSize(0,0,64,128);
       // gfx_drawLine(32,32,32,0);
       // gfx_drawLine(32,32,64,0);
       // gfx_drawLine(32,32,64,32);
@@ -193,6 +194,29 @@ void setup(){
 bool states[2][5];
 
 void loop(){
+  if(io.bscan_down){
+    auto n = midi_base;
+    for(int i=0; i<10; i++){
+      if((1<<i)&io.bscan_down){
+        n+=i;
+        break;
+      }
+    }
+    usbmidi.sendNoteOn(0,n,127);
+    io.bscan_down = 0;
+  }
+  if(io.bscan_up){
+    auto n = midi_base;
+    for(int i=0; i<10; i++){
+      if((1<<i)&io.bscan_up){
+        n+=i;
+        break;
+      }
+    }
+    usbmidi.sendNoteOn(0,n,0);
+    io.bscan_up = 0;
+  }
+
   if(int a = usbmidi.available()){
     uint32 aa = usbmidi.readPacket();
     char *b = (char*)&aa;
