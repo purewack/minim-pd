c090111d2f1b00735b75316caa7013a9c5971611
Damian Nowacki <bobimaster15@gmail.com>
2022-12-02 22:38:58 +0000

    [hardware] minim emu draw gfx buffer

--- minim/src/main.cpp
+++ minim/src/main.cpp
@@ -5,16 +5,30 @@ gfx_t gfx;
 
 #define INT(X) (std::stoi(X))
 
+RenderTexture ctx[5];
+int SS = 2;
+
 int initGFXSystem(){
 	data_buf = (uint8_t*)malloc(sizeof(uint8_t)*512);
-    InitWindow(200*4,200,"M I N I M - Emulator");
+    InitWindow(64*SS*6,64*SS,"M I N I M - Emulator");
     BeginDrawing();
     ClearBackground(BLACK);
     EndDrawing();
+
+	for(int i=0; i<5; i++){
+		ctx[i] = LoadRenderTexture(128,128);
+
+		BeginTextureMode(ctx[i]);
+			ClearBackground(BLACK);
+		EndTextureMode();
+	}
 	return 0;
 }
 
 int endGFXSystem(){
+	for(int i=0; i<5; i++)
+		UnloadRenderTexture(ctx[i]);
+	
     CloseWindow();
 	free(data_buf);
 	return 0;
@@ -25,7 +39,17 @@ void cmd_gfx_on_context(int ctx){
 	context = ctx;
 }
 void cmd_gfx_on_draw(){
-    std::cout << "cmd_gfx_on_draw"<< std::endl;
+	BeginTextureMode(ctx[context]);
+		ClearBackground(BLACK);
+		for(int x=0; x<128; x++){
+			for(int y=0; y<32; y++){
+				if(gfx.fbuf_top[x] & (1<<y))
+					DrawPixel(x,y,WHITE);
+				if(gfx.fbuf_bot[x] & (1<<y))
+					DrawPixel(x,y+32,WHITE);
+			}
+		}
+	EndTextureMode();
 }
 
 int initMidiSystem(){
@@ -58,7 +82,7 @@ void collectSysex(unsigned char b){
 	}
 }
 
-void processSystems(){
+void processMidi(){
 	midiin->getMessage( &midiin_bytes);
 	if(midiin_bytes.size() <= 0) return;
 
@@ -93,10 +117,17 @@ int main(){
 	initGFXSystem();
 	initMidiSystem();
 	while(!WindowShouldClose()){
-	processSystems();
-	BeginDrawing();
-	ClearBackground(BLACK);
-	EndDrawing();
+		processMidi();
+		BeginDrawing();
+			ClearBackground(BLACK);
+			for(int i=0; i<5; i++){
+				DrawTexturePro(ctx[i].texture,
+							(Rectangle){0,0,128,-64},
+							(Rectangle){i*64*SS,0,128*SS,64*SS},
+							(Vector2){0,0}, 0.0f, WHITE);
+			}
+			
+		EndDrawing();
 	}
 	endMidiSystem();
 	endGFXSystem();
